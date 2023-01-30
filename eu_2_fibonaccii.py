21640bbdbf1ff5993fecc2634fa124299ec3a911
MADHAN KUMAR SELVARAJ <dhivyaprimary@gmail.com>
2023-01-30 14:17:33 +0530

    made changes in fibonacci and added veettu manai

--- eu_2_fibonaccii.py
+++ eu_2_fibonaccii.py
@@ -3,9 +3,9 @@
 # @Author  : Madhan Kumar S
 # @Email   : madhan.ks@logically.co.uk
 num_1, num_2 = eval(input("Enter 2 value separated by comma : "))
-limit = eval(input("fibonacci sequence: "))
+limit = eval(input("fibonacci sequence limit: "))
 # print(num_1,num_2)
-style = [num_1,num_2]
+fibonacci_series = [num_1, num_2]
 for test in range(0, limit):
     num_3 = num_1 + num_2
     if num_3 >= limit:
@@ -14,14 +14,14 @@ for test in range(0, limit):
     num_1 = num_2
     num_2 = num_3
     # print(num_3)
-    style.append(num_3)
-# print(style)
+    fibonacci_series.append(num_3)
+print(fibonacci_series)
 output = 0
-for iterate in style:
-    if iterate%2 == 0:
+for iterate in fibonacci_series:
+    if iterate % 2 == 0:
         # print(iterate)
         output = output + iterate
-print("sum of even fibonacci sequence sum of even fibonacci sequence sum of even fibonacci sequence sum of even fibonacci sequence: ",output)
+print("sum of even fibonacci sequence: ",output)
 
 
 
